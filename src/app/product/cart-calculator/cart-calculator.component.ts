92e92e76815df0eff57f29af9847b179cebb00c7
dollypizzle <dolapo.mabawonku@gmail.com>
2022-07-29 18:40:02 +0100

    Project setup

---
+++ src/app/product/cart-calculator/cart-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-calculator',
+  templateUrl: './cart-calculator.component.html',
+  styleUrls: ['./cart-calculator.component.css']
+})
+export class CartCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
