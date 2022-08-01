2208afd216f8323a61fcb6e5ec17106077591132
dollypizzle <dolapo.mabawonku@gmail.com>
2022-08-01 18:42:54 +0100

    Setup index pages

---
+++ src/app/index/index.module.ts
@@ -0,0 +1,31 @@
+import { RouterModule } from '@angular/router';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+import { IndexRoutes } from './index.routing';
+import { IndexComponent } from './index.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    // ProductModule,
+    SharedModule,
+    RouterModule.forChild(IndexRoutes),
+  ],
+  declarations: [
+    IndexComponent,
+    NavbarComponent,
+    LoginComponent,
+    FooterComponent,
+    RegisterComponent,
+  ],
+  schemas: [NO_ERRORS_SCHEMA],
+  exports: [NavbarComponent, FooterComponent],
+  providers: [],
+})
+export class IndexModule {}
